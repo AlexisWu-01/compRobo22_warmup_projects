a62cc6ac78d85f2f988b1817c4809f0f46bb73bc
Alexis Wu <wuxinyi2000@gmail.com>
2022-09-13 13:32:45 -0400

    time-lapse

--- compRobo22_warmup_projects/compRobo22_warmup_projects/drive_square_v0.py
+++ compRobo22_warmup_projects/compRobo22_warmup_projects/drive_square_v0.py
@@ -12,39 +12,25 @@ class drive_square(Node):
         timer_period = 0.1
         self.timer = self.create_timer(timer_period,self.send_msg) #runs at timer_period, then call the call back function
         self.publisher = self.create_publisher(Twist,"cmd_vel",10)#name of topic,"cmd_vel" for moving; queue size
-        self.start_time = None
 
     def send_msg(self):
-        straight = Twist()
-        straight.linear.x = 0.5
-        turn = Twist()
-        turn.angular.z = 0.5
-        if self.start_time is None:
-            self.start_time = time.time()
         # record the current time
-        elapsed = time.time() - self.start_time
-        if elapsed < 2:
-            self.publisher.publish(straight)
-        elif elapsed < 5.14:
-            self.publisher.publish(turn)
-        else:
-            self.start_time = time.time()
-        
+        prev = time.time()
+
 
         # msg = String(data = 'hello') #each msg has a label to go with, String is a datatype
         # msg = String()
         # msg.data = 'hello'
-
-        #straight = Twist(linear=Vector3(x=1.0,y=0.0,z=0.0),angular=Vector3(x=0.0,y=0.0,z=0.0))
-        #turn = Twist(linear=Vector3(x=0.0,y=0.0,z=0.0),angular=Vector3(x=0.0,y=0.0,z=-0.5))
+        straight = Twist(linear=Vector3(x=0.5,y=0.0,z=0.0),angular=Vector3(x=0.0,y=0.0,z=0.0))
+        turn = Twist(linear=Vector3(x=0.0,y=0.0,z=0.0),angular=Vector3(x=0.0,y=0.0,z=-0.5))
         #msg = Twist
         #msg.lin_vel =
 
-        #while (time.time()-prev < 1):
-        #    self.publisher.publish(straight)
-        #prev = time.time()
-        #while (time.time()-prev < 3.14):
-        #    self.publisher.publish(turn)
+        while (time.time()-prev < 2):
+            self.publisher.publish(straight)
+        prev = time.time()
+        while (time.time()-prev < 3.14):
+            self.publisher.publish(turn)
 
 
 

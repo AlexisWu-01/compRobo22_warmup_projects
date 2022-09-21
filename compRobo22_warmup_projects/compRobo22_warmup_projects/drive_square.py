c34a6f0e5a68bba0353d508dc73e57d72e0579ec
AlexisWu-01 <wuxinyi2000@gmail.com>
2022-09-21 14:10:22 -0400

    complete documentation for drive_square

--- compRobo22_warmup_projects/compRobo22_warmup_projects/drive_square.py
+++ compRobo22_warmup_projects/compRobo22_warmup_projects/drive_square.py
@@ -56,13 +56,21 @@ class drive_square(Node):
         # Stores orientation of neato in odometry frame, centered as neato center
         # We only care about z in this case, which is [2] in the list
         self.current_orientation = None
-        # checks if 
+
+        # checks if the neato is bumping into something
         self.bumper_active = False
+        #re-callibrates the odometry frame everytime
         self.callibration = False
+        # change in x,y for a square
         self.deltas = [(0.0,0.0),(1.0,0.0),(1.0,1.0),(0.0,1.0)]
+        #position of neato in odometry frame
         self.corners = [None,None,None,None]
-        self.count = 1 #track which corner to go to next
+        #tracks which corner to go to next
+        self.count = 1 
+
+        # turn 90 degree at each corner, in radians
         self.turn_angle = math.pi/2
+        #target angle in odometry frame
         self.target_angle = None
 
         # state machines to determine neato's move pattern
@@ -71,12 +79,30 @@ class drive_square(Node):
 
 
     def process_bump(self, msg):
+        """Subcribes to bump msg:
+            If any of the bumper sensor is True,
+                set the bumper_active to True.
+    
+        """
         self.bumper_active = (msg.left_front == 1 or \
                               msg.left_side == 1 or \
                               msg.right_front ==1 or \
                               msg.right_side == 1)
 
+
     def check_odom (self,msg):
+        """
+        Subcribes to neato location and orientation
+            in odometry frame.
+
+        Vars:
+            current_position: position in neato frame. 
+                calls through self.current_position.x and self.current_position.y.
+            current_orientation: orientation in neato frame.
+                transformed to neato center in odometry frame.
+                class through self.current_orientation[2] (which is z).
+
+        """
         self.current_position = msg.pose.pose.position
         self.current_orientation = euler_from_quaternion(msg.pose.pose.orientation.x,
                                     msg.pose.pose.orientation.y,
@@ -84,40 +110,58 @@ class drive_square(Node):
                                     msg.pose.pose.orientation.w)
 
     def callibrate_odometry(self):
+        """
+        Transforms relative position (self.deltas) to absolute position in neato frame.
+
+        """
+        #make sure the points are looping in list index 0 to 3
         if self.count < 1:
                     pointer = 3
         else:
             pointer = self.count - 1
+
         cur_x = self.current_position.x
         cur_y = self.current_position.y
         for i in range(len(self.deltas)):
+            #sets absolute position in odometry frame
             self.corners[i] = (self.deltas[i][0]+cur_x-self.deltas[pointer][0],self.deltas[i][1]+cur_y-self.deltas[pointer][1])
 
+        # sets callibration to True so it will not callibrate again.
         self.callibration = True
 
     def send_msg(self):
+        """
+        Publishes velocity information in each timer_period
+        """
         if not self.callibration:
-            #callibrates the start odom frame (real_world)
+            #callibrates the odom frame (real_world)
             print('start callibration')
             try:
                 self.callibrate_odometry()
             except:
                 pass
-
+        # starts velocity command, everything defaults to zero.
         msg = Twist()
         try:
+            # displacement in x and y direction in odometry frame.
             error_x = self.corners[self.count][0] - self.current_position.x
             error_y = self.corners[self.count][1] - self.current_position.y
+            # distance between current and target position.
             error_distance = math.sqrt(error_x**2+error_y**2)
+
             if self.to_turn:
+                # if state set to turn, runs this script to turn.
                 if not self.target_angle:
+                    # initialize target_angle just once.
                     self.target_angle = self.turn_angle + self.current_orientation[2]
                 angle_to_turn = self.target_angle - self.current_orientation[2]
                 if angle_to_turn > 5e-2:
-                    msg.angular.z = self.kp * angle_to_turn * self.CONSTANT_ANGULAR_SPEED
+                    # sets angular velocity proportional, but not lower than 0.05 rad/s
+                    msg.angular.z = max(self.kp * angle_to_turn * self.CONSTANT_ANGULAR_SPEED,0.05)
                     print('turning at',msg.angular.z)
                     print(angle_to_turn,"to turn")
                 else:
+                    # if target angle is reached, switch mode and reset callibration and initialization.
                     msg.angular.z = 0.0
                     self.to_go = True
                     self.to_turn = False
@@ -126,16 +170,23 @@ class drive_square(Node):
 
 
             elif self.to_go:
-                msg.linear.x = self.kp*self.CONSTANT_LINEAR_SPEED*error_distance
+                # if state set to go straight, run this script to go.
+
+                #when target position is not reached, go proportional speed but with low limits..
+                msg.linear.x = max(self.kp*self.CONSTANT_LINEAR_SPEED*error_distance, 0.05)
+
                 print('going at',msg.linear.x)
                 print(error_distance,"to go")
+
                 if error_distance < 5e-2:
+                    # when error position is reached, switch mode and reset initilization
                     msg.linear.x = 0.0
                     self.to_turn = True
                     self.to_go = False
                     self.target_angle = None
                     print('head to next point')
                     if self.count < 3:
+                        #update to next corner
                         self.count += 1
                     else:
                         self.count = 0
@@ -143,6 +194,7 @@ class drive_square(Node):
 
         except:
             pass
+        #publishes velocity
         self.publisher.publish(msg)
 
 
